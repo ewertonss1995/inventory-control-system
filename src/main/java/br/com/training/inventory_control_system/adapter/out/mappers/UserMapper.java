4560d541737e53e6dc3882eca2ba1625cee06659
ewerton.sd.silva <ewerton.sd.silva@bradesco.com.br>
2025-09-23 19:15:49 -0300

    [TRNG-0011] - Ajuste em objetos de retorno

---
+++ src/main/java/br/com/training/inventory_control_system/adapter/out/mappers/UserMapper.java
@@ -0,0 +1,28 @@
+package br.com.training.inventory_control_system.adapter.out.mappers;
+
+import br.com.training.inventory_control_system.adapter.in.controllers.product.request.ProductRequest;
+import br.com.training.inventory_control_system.adapter.out.responses.GetProductResponse;
+import br.com.training.inventory_control_system.domain.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "productId", ignore = true)
+    @Mapping(target = "totalPrice", ignore = true)
+    @Mapping(target = "updateDate", ignore = true)
+    @Mapping(target = "category.categoryId", source = "categoryId")
+    Product toEntity(ProductRequest request);
+
+    GetProductResponse toGetProductResponse(Product entity);
+
+    List<GetProductResponse> toGetProductResponseList(List<Product> entities);
+
+    @Mapping(target = "registrationDate", ignore = true)
+    @Mapping(target = "category.categoryId", source = "categoryId")
+    void updateEntityFromRequest(ProductRequest request, @MappingTarget Product entity);
+}
